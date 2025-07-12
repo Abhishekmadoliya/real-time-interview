6e65b95d9e25686c3a06d3b039e3a0972ec69adf
Abhishek madoliya <abhishekmadoliya@gmail.com>
2025-07-12 20:31:02 +0530

    files uploaded

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './componenets/navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-3xl font-bold  text-center underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
